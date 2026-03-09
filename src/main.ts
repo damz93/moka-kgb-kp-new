cd89526534b4b47bd6bc1bd95a0e72c13fceb7f7
Dedy A Muzawwir <dedy.alimmuzawwir@gmail.com>
2026-03-09 10:34:25 +0800

    update_

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import Swal from 'sweetalert2';
 
 // --- CONFIGURATION ---
 // const GAS_URL = 'https://script.google.com/macros/s/AKfycbxhrCUKHLpYLeTYRFK4xMCaegKcehMWj2l7PoAVHIzByWvrWt7nPqbY6G0CN4yrd8v0tA/exec';
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbxYznBsMDimCFtsiTVM0dXytPKvscUt754VH8k87GcrcrOpaThqCrNTuAao-IQSB4cQeA/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbzW9DLKyh_mkColr-vEM5fN9UwjBeiTRnHdk6Gz6PvyHppY140UFNg5gpEjpqlo0QKL-w/exec';
 
 // --- STATE MANAGEMENT ---
 let currentPage = 'home';
