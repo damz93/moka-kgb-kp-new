90cac5129fd996034cf1ae415e3db9662f7e7cf7
damz <dedy.alimmuzawwir@gmail.com>
2026-03-14 16:45:45 +0800

    cek-login5

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import Swal from 'sweetalert2';
 // const GAS_URL = 'https://script.google.com/macros/s/AKfycbx18wfWpMC5w3lNZ8gIY24L-FbllGFroFM0FJWEm1kHFdGxwjOPJCcB4JU4sPtMjsbf-A/exec';
 // const GAS_URL = 'https://script.google.com/macros/s/AKfycbyC7hYE5w1_hB3JnSJ_Z_LuEcLaBFChTvHgH30mHFwm2387LBNuJX9zyRWN8YqWH5yJfw/exec';
 //const GAS_URL = 'https://script.google.com/macros/s/AKfycbzorsD488lhGHkTq1tmj1JxhgaPZwcXC1tySDzFCiHMRYnu8eRs-3KdcBmjK3nKJLq8Cg/exec';
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbxx9HDOwfDSxN4O6S9VNQwpgi97spOM7XlXIV2HDVF6w6e_wh95dWEy7gMBgxyCrVzYhQ/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbxviWAQuc53YssIjf4OeG-d-m8wcB4gmFkMBRsQ6vL71yysmxJ4ua-CEGIEp7wVFFoG9w/exec';
 
 let currentPage = 'home';
 let isAdmin = !!localStorage.getItem('moka_token');
